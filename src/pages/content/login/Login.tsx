9286c78a0d6c4e145f6e6313a19b98b4b383dfe9
dennissherb <qkweint@gmail.com>
2025-02-18 20:57:31 +0200

    design for login page

--- src/pages/content/login/Login.tsx
+++ src/pages/content/login/Login.tsx
@@ -1,6 +1,13 @@
+// Login.tsx
 import { ChangeEvent, useState } from "react";
 import styles from "./Login.module.scss";
 import { useNavigate } from "react-router-dom";
+import Header from "src/pages/containers/gdiMain/header/Header";
+
+
+const toggleMenu = () => {
+  console.log("click");
+};
 
 function isValidUrl(str: string): boolean {
   try {
@@ -17,25 +24,22 @@ export default function Login() {
   const [suggestions, setSuggestions] = useState([] as string[]);
   const navigate = useNavigate();
 
-  // Sample suggestions
   const options = ["https://gdi.gloki.contact"];
 
-  // Handle input change
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setInputValue(value);
     if (value) {
       setSuggestions(
-        options.filter((option) =>
-          option.toLowerCase().includes(value.toLowerCase())
-        )
+          options.filter((option) =>
+              option.toLowerCase().includes(value.toLowerCase())
+          )
       );
     } else {
       setSuggestions([]);
     }
   };
 
-  // Handle suggestion click
   const handleSuggestionClick = (suggestion: string) => {
     setInputValue(suggestion);
     setSuggestions([]);
@@ -45,7 +49,7 @@ export default function Login() {
     const pad = new Uint8Array(32);
     crypto.getRandomValues(pad);
     setGloki(
-      Array.from(pad, (value) => value.toString(16).padStart(2, "0")).join("")
+        Array.from(pad, (value) => value.toString(16).padStart(2, "0")).join("")
     );
   };
 
@@ -53,42 +57,52 @@ export default function Login() {
     if (gloki.length > 2 && isValidUrl(inputValue)) {
       localStorage.setItem("server", inputValue);
       localStorage.setItem("agent", gloki);
-      const redirectTo =
-        sessionStorage.getItem("redirectAfterLogin") || "/main";
+      const redirectTo = sessionStorage.getItem("redirectAfterLogin") || "/main";
       sessionStorage.removeItem("redirectAfterLogin");
       navigate(redirectTo, { replace: true });
     }
   };
 
   return (
-    <div className={styles["login-container"]}>
-      <div>gloKi: {gloki}</div>
-      <button onClick={generateGloki}>Create</button>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Start typing..."
-      />
-      {suggestions.length > 0 && (
-        <ul>
-          {suggestions.map((suggestion, index) => (
-            <li
-              key={index}
-              onClick={() => handleSuggestionClick(suggestion)}
-              onMouseOver={(e) =>
-                (e.currentTarget.style.backgroundColor = "#eee")
-              }
-              onMouseOut={(e) =>
-                (e.currentTarget.style.backgroundColor = "#f9f9f9")
-              }
-            >
-              {suggestion}
-            </li>
-          ))}
-        </ul>
-      )}
-      <button onClick={login}>Login</button>
-    </div>
+      <div className={styles.loginContainer}>
+        <Header onMenuToggle={toggleMenu} />
+        <div className={styles.topElement}>
+          <div className={styles.logo}>Welcome</div>
+          <button className={styles.createButton} onClick={generateGloki}>
+            Create
+          </button>
+          <div className={styles.glokiId}>{gloki}</div>
+        </div>
+
+        <div className={styles.inputSection}>
+          <div className={styles.title}>Enter server:</div>
+          <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="e.g. https://gdi.gloki.contact"
+          />
+          {suggestions.length > 0 && (
+              <ul className={styles.suggestions}>
+                {suggestions.map((suggestion, index) => (
+                    <li
+                        key={index}
+                        onClick={() => handleSuggestionClick(suggestion)}
+                    >
+                      {suggestion}
+                    </li>
+                ))}
+              </ul>
+          )}
+          <button className={styles.loginButton} onClick={login}>
+            Login
+          </button>
+        </div>
+
+        <div className={styles.bottomElement}>
+          <div className={styles.footer}></div>
+          <div className={styles.footerOutline}></div>
+        </div>
+      </div>
   );
 }
